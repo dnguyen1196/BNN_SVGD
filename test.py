5ebaef22c0162a4de6da93f1698041b5c66bbb93
ducnguyentufts <manh.nguyen@tufts.edu>
2019-04-11 00:00:51 -0400

    Can dump and load pkl object

--- test.py
+++ test.py
@@ -37,50 +37,9 @@ test_loader = torch.utils.data.DataLoader(
 
 
 
-num_networks = 1
-model = CovNet_SVGD(image_set="CIFAR-10", num_networks=num_networks)
-
-optimizer = optim.SGD(model.parameters(), lr=0.001)
-# # optimizer = optim.Adagrad(model.parameters(), lr=1)
-
-count = 1
-
-def train_model(model, optimizer, train_loader, device):
-    total_loss = 0.
-    num_batches = 0
-    count = 0
-    correct = 0
-    total = 0
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        X, y = data.to(device), target.to(device)
-
-        y_onehot = torch.FloatTensor(y.shape[0], num_classes)
-        y_onehot.zero_()
-        y_onehot.scatter_(1, y.view(-1,1), 1.)
-
-        # print(X.shape)
-        # print(y.shape)
-        optimizer.zero_grad()
-
-        loss = model.loss(X, y_onehot)
-        loss.backward()
-
-        outputs = model.predict_average(X)
-        optimizer.step()
-
-        total_loss += loss
-        num_batches += 1
-
-        _, predicted = outputs.max(1)
-        total += y.size(0)
-        correct += predicted.eq(y).sum().item()
-
-        count += 1
-        # if loss > 10000:
-        #     print(loss)
-
-    return total_loss/num_batches, correct/total
+model = None
+with open("cifar-10-model.pkl", "rb") as f:
+    model = pickle.load(f)
 
 
 def evaluate_test_set(model, train_loader, device):
@@ -115,15 +74,3 @@ def evaluate_test_set(model, train_loader, device):
 
 avg_loss_test, accuracy_test = evaluate_test_set(model, test_loader, device)
 print("Init: evaluate_test_set-avg-loss = {}, evaluate_test_set-accuracy = {}".format(avg_loss_test, accuracy_test))
-
-for epoch in range(num_epochs):
-    avg_loss_train, accuracy_train = train_model(model, optimizer, train_loader, device)
-    avg_loss_test, accuracy_test = evaluate_test_set(model, test_loader, device)
-    print("epoch {}, train_model-avg-loss = {}, train_model-accuracy = {}, evaluate_test_set-avg-loss = {}, evaluate_test_set-accuracy = {}"\
-        .format(epoch, avg_loss_train, accuracy_train, avg_loss_test, accuracy_test))
-
-
-model_file = "cifar-10-model.pkl"
-
-with open(model_file, "wb") as f:
-    pickle.dump(model, f)
