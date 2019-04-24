ffbd66950d4f848fbe3cdc8cb130c141b4ff0fca
dnguyen1196 <ducnguyenmanh96@gmail.com>
2019-04-24 13:42:04 -0400

    results from cifar10

--- run_cnnsvgd_test.sh
+++ run_cnnsvgd_test.sh
@@ -3,13 +3,14 @@ source activate py3.6_torch1.0_pyro0.3
 
 opts="-p batch -c 2 --mem=16000 --time=4-00:00:00 --mail-type=ALL --mail-user=$USER"
 
-read outdir   # output directory
+outdir="./cifar10-cnn-svgd-checkpoint"
+
 echo "Output directory: $outdir"
 
 script="python -u cnn_svgd_test.py"
 
 outs="--output=$outdir/cnn_svgd.out --error=$outdir/cnn_svgd.err"
 
-sbatch $opts $outs --wrap="$script"
+sbatch $opts $outs --wrap="python -u cnn_svgd_test.py"
 
-sleep 1
+
